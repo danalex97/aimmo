e71adc7aaad894d1d9e5b56efadeff4bb4c1ccce
Alexandru Dan <dan.alex97@yahoo.com>
2017-07-28 11:23:49 +0100

    Trying to set the environment variables that seem to be missing from the docker container by hand.

--- aimmo-game-creator/service.py
+++ aimmo-game-creator/service.py
@@ -7,6 +7,11 @@ from worker_manager import WORKER_MANAGERS
 
 def main():
     logging.basicConfig(level=logging.DEBUG)
+
+    if "aimmo-game-creator" in os.environ.get("HOSTNAME", ''):
+        os.environ["GAME_API_URL"] = "https://staging-dot-decent-digit-629.appspot.com/aimmo/api/games/"
+        os.environ["WORKER_MANAGER"] = "kubernetes"
+
     WorkerManagerClass = WORKER_MANAGERS[os.environ.get('WORKER_MANAGER', 'local')]
     worker_manager = WorkerManagerClass(os.environ.get('GAME_API_URL',
                                         'http://localhost:8000/players/api/games/'))
